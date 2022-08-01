220222bef22137eb602b55a486c30524821761e0
BELHAJ Manel <belhaj343@gmail.com>
2022-08-01 15:07:30 +0100

    added basic components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <h2 className="text-center">
-        Rick & morty <span className="text-primary">WiKi</span>
-      </h2>
+      <h1 className="text-center ubuntu my-4">
+        Rick & Morty <span className="text-primary">WiKi</span>
+      </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+              <Filters></Filters>
+          </div>
+          <div className="col-8">
+            <div className="row">      
+              <Cards/>
+              <Cards/>
+              <Cards/>
+            </div>
+              
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <React.Fragment>
+      <div className="col-4">Cards</div>
+    </React.Fragment>
+  )
+}
+
+export default Cards;
